482cd692217e66f62bcf01fe59fae816771d932f
humaidi <humaidizakaria1998@gmail.com>
2022-06-23 23:35:15 +0700

    Membuat API register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,26 +5,81 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        // jika email kosong/tidak diisi
-        if (!$request->email) {
-            return "Email tidak boleh kosong";
+        // validasi inputan
+        $validasi = Validator::make($request->all(),  [
+            'email' => 'required',
+            'password' => 'required|min:6',
+        ]);
+
+        // ambil errornya
+        if ($validasi->fails()) {
+            return $this->error($validasi->errors()->first());
         }
 
         // ambil data user berdasarkan email
         $user = User::where('email', $request->email)->first();
 
 
-
         // jika ada yang cocok maka
         if ($user) {
-            return "Selamat datang " . $user->name;
+
+            // cek password
+            if (password_verify($request->password, $user->password)) {
+                return $this->success("Selamat datang " . $user->name, $user);
+            } else {
+                return $this->error("Password tidak cocok");
+            }
         }
 
-        return "User tidak ditemukan";
+        return $this->error("Email tidak ditemukan");
+    }
+
+    public function register(Request $request)
+    {
+        // validasi inputan
+        $validasi = Validator::make($request->all(),  [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'phone' => 'required|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        // ambil errornya
+        if ($validasi->fails()) {
+            return $this->error($validasi->errors()->first());
+        }
+
+        // masukan data register user ke database
+        $user = User::create(array_merge($request->all(), [
+            'password' => bcrypt($request->password)
+        ]));
+        if ($user) {
+            return $this->success("Akun berhasil dibuat", $user);
+        } else {
+            return $this->error("Terjadi kesalahan");
+        }
+    }
+
+    public function success($message = 'success', $data)
+    {
+        return response()->json([
+            'code' => 200,
+            'message' => $message,
+            'data' => $data,
+        ]);
+    }
+
+    public function error($message)
+    {
+        return response()->json([
+            'code' => 400,
+            'message' => $message,
+        ], 400);
     }
 }
